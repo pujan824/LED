7c5e2e598c4d8d35dc0e2cf00fff5e35f0aaa876
pujan824 <pujan824@gmail.com>
2018-12-03 22:11:49 -0500

    Create confetti.ino

---
+++ confetti/confetti.ino
@@ -0,0 +1,71 @@
+#include "FastLED.h"                                          // FastLED library. Preferably the latest copy of FastLED 2.1.
+
+#if FASTLED_VERSION < 3001000
+#error "Requires FastLED 3.1 or later; check github for latest code."
+#endif
+
+#define LED_DT 1                                             // Data pin
+#define COLOR_ORDER BGR                                      
+#define LED_TYPE WS2811                                      
+#define NUM_LEDS 50                                           
+
+// Initialize changeable global variables.
+uint8_t max_bright = 128;                                     
+struct CRGB leds[NUM_LEDS];                                   
+unsigned long previousMillis;                                 
+
+
+// Define variables used by the sequences.
+uint8_t  thisfade = 4;                                        // How quickly does it fade? Lower = slower fade rate.
+int       thishue = 50;                                       // Starting hue.
+uint8_t   thisinc = 1;                                        // Incremental value for rotating hues
+uint8_t   thissat = 100;                                      // The saturation, where 255 = brilliant colours.
+uint8_t   thisbri = 255;                                      // Brightness of a sequence. Remember, max_bright is the overall limiter.
+int       huediff = 256;                                      // Range of random #'s to use for hue
+uint8_t thisdelay = 5;                                        // We don't need much delay (if any)
+
+
+void setup() {
+  delay(1000);                                               
+  Serial.begin(9600);
+
+  LEDS.addLeds<LED_TYPE, LED_DT, COLOR_ORDER>(leds, NUM_LEDS);    
+
+  FastLED.setBrightness(max_bright);
+  set_max_power_in_volts_and_milliamps(5, 500);               //power management set at 5V, 500mA.
+}
+
+
+void loop () {
+  ChangeMe();                                           
+        
+
+  EVERY_N_MILLISECONDS(thisdelay) {                          
+    confetti();
+  }
+  FastLED.show();  
+}
+
+
+
+void confetti() {                                             // random colored speckles that blink in and fade smoothly
+  fadeToBlackBy(leds, NUM_LEDS, thisfade);                    // Low values = slower fade.
+  int pos = random16(NUM_LEDS);                               // Pick an LED at random.
+  leds[pos] += CHSV((thishue + random16(huediff))/4 , thissat, thisbri);  // I use 12 bits for hue so that the hue increment isn't too quick.
+  thishue = thishue + thisinc;                                // It increments here.
+} // confetti()
+
+
+void ChangeMe() {                                             // A time (rather than loop) based demo sequencer. This gives us full control over the length of each sequence.
+  uint8_t secondHand = (millis() / 1000) % 15;                // IMPORTANT!!! Change '15' to a different value to change duration of the loop.
+  static uint8_t lastSecond = 99;                             // Static variable, means it's only defined once. This is our 'debounce' variable.
+  if (lastSecond != secondHand) {                             // Debounce to make sure we're not repeating an assignment.
+    lastSecond = secondHand;
+    switch(secondHand) {
+      case  0: thisinc=1; thishue=192; thissat=255; thisfade=2; huediff=256; break;  // You can change values here, one at a time , or altogether.
+      case  5: thisinc=2; thishue=128; thisfade=8; huediff=64; break;
+      case 10: thisinc=1; thishue=random16(255); thisfade=1; huediff=16; break;      // Only gets called once, and not continuously for the next several seconds. Therefore, no rainbows.
+      case 15: break;                                                                // Here's the matching 15 for the other one.
+    }
+  }
+}
